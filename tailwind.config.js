e38755a0c86fa955089c91c2896bbe79e43a882e
Sebas1781 <smontielmoreno13@micorreo.upp.edu.mx>
2026-01-02 21:47:02 -0600

    Fix checkbox and radio styles in production

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,17 @@
+/** @type {import('tailwindcss').Config} */
 export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+  safelist: [
+    // Asegurar que los estilos de checkbox y radio no se eliminen en producción
+    'accent-green-600',
+    'accent-[#00A859]',
   ],
 }
 
